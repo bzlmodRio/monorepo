94d9bc22fd3f33d9653d7966960ee20b4514f67b
PJ Reiniger <pj.reiniger@gmail.com>
2023-05-04 15:55:22 -0400

    Sanity

--- monorepo_tools/common.sh
+++ monorepo_tools/common.sh
@@ -1,7 +1,5 @@
 
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo $SCRIPT_DIR
 
 MONOREPO_BASE=$SCRIPT_DIR/..
-echo $MONOREPO_BASE
--- monorepo_tools/generate_subprojects.sh
+++ monorepo_tools/generate_subprojects.sh
@@ -1,21 +1,25 @@
-#!/bin/bash -e
-set -e
+#!/bin/bash
+# set -e
 
 source ./project_list.sh
 source ./common.sh
 
 
-for project in "${LIBRARY_PROJECTS[@]}"; do    
-    echo "Generating $project"
-    cd $MONOREPO_BASE/$project/generate
-    bazel run //:generate -- --force_tests && buildifier --lint=fix -r .. 
-    err=$?
-    echo "CODE----------------- $err"
-    if [[ $err -ne 0 ]]; then
-        echo "FAILED"
-        exit $err
-    fi;
-    
-    cd $MONOREPO_BASE/$project
-    buildifier --lint=fix -r .
+for project in "${PROJECTS[@]}"; do    
+    GENERATION_DIR=$MONOREPO_BASE/$project/generate
+    if [ -f $GENERATION_DIR/generate.py ] ; then
+        echo "Generating $project"
+        cd $GENERATION_DIR
+        bazel run //:generate 2> /dev/null
+        err=$?
+        if [[ $err -ne 0 ]]; then
+            echo "FAILED"
+            exit $err
+        fi;
+        
+        cd $MONOREPO_BASE/$project
+        buildifier --lint=fix -r .
+    else
+        echo "Project $project does not seem generate-able"
+    fi
 done
