5d5116d2cb87d9597b564be357fc0b1e93cb6ac8
PJ Reiniger <pj.reiniger@gmail.com>
2023-05-05 15:41:51 -0400

    More gen

--- monorepo_tools/format_subprojects.sh
+++ monorepo_tools/format_subprojects.sh
@@ -5,8 +5,10 @@ source ./common.sh
 
 
 for project in "${LIBRARY_PROJECTS[@]}"; do    
-    cd $MONOREPO_BASE/$project/generate
-    python3 -m black *.py
+    cd $MONOREPO_BASE/$project
+    python3 -m autoflake $(git ls-files '*.py') --in-place --remove-unused-variable --remove-all-unused-imports
+    python3 -m black .
+    # python3 -m pylint $(git ls-files '*.py')
 
     cd $MONOREPO_BASE/$project
     buildifier --lint=fix -r .
