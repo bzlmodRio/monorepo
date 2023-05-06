329dc7ff39c5d92e08f900f6790673c88d9c621b
PJ Reiniger <pj.reiniger@gmail.com>
2023-05-06 14:30:39 -0400

    Update

---
+++ monorepo_tools/auto_update.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# set -e
+
+source ./project_list.sh
+source ./common.sh
+
+
+for project in "${PROJECTS[@]}"; do    
+    GENERATION_DIR=$MONOREPO_BASE/$project/generate
+    if [ -f $GENERATION_DIR/auto_update.py ] ; then
+        echo "Generating $project"
+        cd $GENERATION_DIR
+        bazel run //:auto_update # 2> /dev/null
+        err=$?
+        if [[ $err -ne 0 ]]; then
+            echo "FAILED"
+            exit $err
+        fi;
+        
+        cd $MONOREPO_BASE/$project
+        buildifier --lint=fix -r .
+    else
+        echo "Project $project does not seem generate-able"
+    fi
+done
---
+++ monorepo_tools/clear_tags.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source ./project_list.sh
+source ./common.sh
+
+for project in "${PROJECTS[@]}"; do  
+    cd $MONOREPO_BASE/$project
+    git tag -l | xargs git tag -d
+done
