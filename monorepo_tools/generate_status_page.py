50f0763c496b3eed77af4de60916c6545f0ce07b
PJ Reiniger <pj.reiniger@gmail.com>
2023-05-07 23:28:33 -0400

    More

--- monorepo_tools/generate_status_page.py
+++ monorepo_tools/generate_status_page.py
@@ -28,6 +28,11 @@ def main():
         output += f'<a href="https://github.com/bzlmodRio/{project}/tree/{branch}"><img src="https://github.com/bzlmodRio/{project}/actions/workflows/lint.yml/badge.svg?branch={branch}"></a>\n'
         output += f'<a href="https://github.com/bzlmodRio/{project}/tree/{branch}"><img src="https://github.com/bzlmodRio/{project}/actions/workflows/auto_update.yml/badge.svg?branch={branch}"></a>\n'
         output += f'<a href="https://github.com/bzlmodRio/{project}/tree/{branch}"><img src="https://github.com/bzlmodRio/{project}/actions/workflows/publish.yml/badge.svg?branch=2024.DUMMY"></a>\n'
+        output += f'Main:'
+        output += f'<a href="https://github.com/bzlmodRio/{project}"><img src="https://github.com/bzlmodRio/{project}/actions/workflows/build.yml/badge.svg"></a>\n'
+        output += f'<a href="https://github.com/bzlmodRio/{project}"><img src="https://github.com/bzlmodRio/{project}/actions/workflows/lint.yml/badge.svg"></a>\n'
+        output += f'<a href="https://github.com/bzlmodRio/{project}"><img src="https://github.com/bzlmodRio/{project}/actions/workflows/auto_update.yml/badge.svg"></a>\n'
+        output += f'<a href="https://github.com/bzlmodRio/{project}"><img src="https://github.com/bzlmodRio/{project}/actions/workflows/publish.yml/badge.svg"></a>\n'
         output += "<br>"
 
     with open("/home/pjreiniger/git/bzlmodRio/monorepo/status.html", "w") as f:
