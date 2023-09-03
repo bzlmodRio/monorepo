e81fb8b81aa64cfa72ab1d074405d06012a1e492
PJ Reiniger <pj.reiniger@gmail.com>
2023-09-02 21:42:48 -0400

    UPdate autogen

---
+++ monorepo_tools/git/add_commit_push_regen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source ./project_list.sh
+source ./common.sh
+
+for project in "${PROJECTS[@]}"; do  
+    cd $MONOREPO_BASE/$project
+    git add .
+    git commit -m "Update autogen"
+done
+
+cd $MONOREPO_BASE
+git add .
+git commit -m "Update autogen"
