2d2aa1a27899c45c8148aed5669ba91c7f7e033e
PJ Reiniger <pj.reiniger@gmail.com>
2024-01-07 18:02:31 -0500

    Update versions

--- monorepo_tools/git/push_projects.sh
+++ monorepo_tools/git/push_projects.sh
@@ -3,10 +3,10 @@
 source ./project_list.sh
 source ./common.sh
 
-git push
-
 for project in "${PROJECTS[@]}"; do
     echo "Pushing $project"
     cd $MONOREPO_BASE/$project
     git push
 done
+
+git push
