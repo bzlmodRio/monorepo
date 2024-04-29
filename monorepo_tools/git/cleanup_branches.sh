46a782fab0cd7fd1166b350c9834e5452cc8830b
PJ Reiniger <pj.reiniger@gmail.com>
2024-04-29 02:10:57 -0400

    More updates

--- monorepo_tools/git/cleanup_branches.sh
+++ monorepo_tools/git/cleanup_branches.sh
@@ -5,6 +5,6 @@ source ./common.sh
 
 for project in "${PROJECTS[@]}"; do  
     cd $MONOREPO_BASE/$project
-    # git add .
-    git commit -m $1
+    pwd
+    git goscleanup
 done
--- monorepo_tools/git/create_pull_requests.sh
+++ monorepo_tools/git/create_pull_requests.sh
@@ -6,5 +6,5 @@ source ./common.sh
 for project in "${LIBRARY_PROJECTS[@]}"; do  
     cd $MONOREPO_BASE/$project
     branch_name=`git rev-parse --abbrev-ref HEAD`
-    python3 $MONOREPO_BASE/monorepo_tools/git/create_pull_requests.py $project $branch_name "Use explicit build files for downloaded artifacts"
+    python3 $MONOREPO_BASE/monorepo_tools/git/create_pull_requests.py $project $branch_name $1
 done
