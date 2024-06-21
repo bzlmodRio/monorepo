2b40063f6fdcdd95955e2f4af706f3944e456b98
PJ Reiniger <pj.reiniger@gmail.com>
2024-06-21 16:31:11 -0400

    Regen

---
+++ monorepo_tools/update_bzlmod_lockfile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# set -e
+
+source ./project_list.sh
+source ./common.sh
+
+FAILURES=""
+
+for project in "${PROJECTS[@]}"; do    
+    cd $MONOREPO_BASE/$project
+    bazel build //... --nobuild --enable_bzlmod --lockfile_mode=update -k
+    if [ $? -ne 0 ]; then
+        echo "Failed"
+        FAILURES="$FAILURES $project"
+    fi
+    bazel shutdown
+    
+    cd $MONOREPO_BASE/$project/tests
+    bazel build //... --nobuild --enable_bzlmod --lockfile_mode=update -k
+    if [ $? -ne 0 ]; then
+        echo "Failed"
+        FAILURES="$FAILURES $project"
+    fi
+    bazel shutdown
+done
+
+echo "Failed: " $FAILURES
