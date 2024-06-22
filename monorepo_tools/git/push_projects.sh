3958b7bb3f5a8c7cbf4143a383b78f44c82f3bf1
PJ Reiniger <pj.reiniger@gmail.com>
2024-06-22 01:17:34 -0400

    Better

--- monorepo_tools/git/push_projects.sh
+++ monorepo_tools/git/push_projects.sh
@@ -3,6 +3,7 @@
 source ./project_list.sh
 source ./common.sh
 
+# for project in "${LIBRARY_PROJECTS[@]}"; do
 for project in "${PROJECTS[@]}"; do
     echo "Pushing $project"
     cd $MONOREPO_BASE/$project
