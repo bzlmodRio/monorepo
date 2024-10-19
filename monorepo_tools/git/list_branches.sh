406c10df9d004418dda4144a091073064eceb979
PJ Reiniger <pj.reiniger@gmail.com>
2024-10-19 01:49:07 -0400

    More

--- monorepo_tools/git/list_branches.sh
+++ monorepo_tools/git/list_branches.sh
@@ -3,11 +3,15 @@
 source ./project_list.sh
 source ./common.sh
 
+EXTRA_ARGS=""
+
+clear;
+
 for project in "${PROJECTS[@]}"; do  
     cd $MONOREPO_BASE/$project
     pwd
-    echo "   Got" `git branch | wc -l` "branches"
+    echo "   Got" `git branch $EXTRA_ARGS | wc -l` "branches"
     echo -e "\n"
-    git branch -vv
+    git --no-pager branch $EXTRA_ARGS -vv
     echo -e "\n"
 done
