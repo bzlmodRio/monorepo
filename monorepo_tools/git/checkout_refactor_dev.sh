03db142341c67ac8b4e8b54b4e2ad54c7cf70c9d
PJ Reiniger <pj.reiniger@gmail.com>
2024-11-05 02:08:25 -0500

    Updates

--- monorepo_tools/git/checkout_refactor_dev.sh
+++ monorepo_tools/git/checkout_refactor_dev.sh
@@ -3,7 +3,9 @@
 source ./project_list.sh
 source ./common.sh
 
-for project in "${PROJECTS[@]}"; do  
+REFACTOR_DEV_PROJECTS=("${RULES_PROJECTS[@]}" "${LIBRARY_PROJECTS[@]}" )
+
+for project in "${REFACTOR_DEV_PROJECTS[@]}"; do  
     cd $MONOREPO_BASE/$project
     pwd
     git checkout refactor_dev
