3f860fa15ddff3694288fcd0d2464c9006fd68ef
PJ Reiniger <pj.reiniger@gmail.com>
2024-11-20 00:03:55 -0500

    More updates

--- monorepo_tools/git/push_projects.sh
+++ monorepo_tools/git/push_projects.sh
@@ -3,8 +3,10 @@
 source ./project_list.sh
 source ./common.sh
 
+PUSH_PROJECTS=("${PROJECTS[@]}" "wpilibsuite_gen" )
+
 # for project in "${LIBRARY_PROJECTS[@]}"; do
-for project in "${PROJECTS[@]}"; do
+for project in "${PUSH_PROJECTS[@]}"; do
     echo "Pushing $project"
     cd $MONOREPO_BASE/$project
     git push
