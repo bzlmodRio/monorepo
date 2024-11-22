337d657d7228108581f718fe8608e4afc2efbce1
PJ Reiniger <pj.reiniger@gmail.com>
2024-11-21 19:08:36 -0500

    Updates

--- monorepo_tools/git/create_pull_requests.sh
+++ monorepo_tools/git/create_pull_requests.sh
@@ -8,11 +8,9 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-# PR_PROJECTS=("${RULES_PROJECTS[@]}" "${LIBRARY_PROJECTS[@]}" )
-PR_PROJECTS=("${LIBRARY_PROJECTS[@]}" )
+PR_PROJECTS=("${RULES_PROJECTS[@]}" "${LIBRARY_PROJECTS[@]}" )
+# PR_PROJECTS=("${LIBRARY_PROJECTS[@]}" )
 
-# for project in "${LIBRARY_PROJECTS[@]}"; do  
-# for project in "${RULES_PROJECTS[@]}"; do  
 for project in "${PR_PROJECTS[@]}"; do  
     cd $MONOREPO_BASE/$project
     branch_name=`git rev-parse --abbrev-ref HEAD`
